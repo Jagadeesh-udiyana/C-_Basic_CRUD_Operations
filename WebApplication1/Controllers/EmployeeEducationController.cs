6031568e7bb075f2cda682cf98620200053ad84a
Brindha <brindha.v@elitehrms.com>
2026-05-28 16:44:02 +0530

    sample post and get apis with sp for backup to revesion

---
+++ WebApplication1/Controllers/EmployeeEducationController.cs
@@ -0,0 +1,413 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Xml;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeEducationController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public EmployeeEducationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetAllEmployees")]
+
+        public ModelEmpEducationList GetAllEmployees()
+        {
+
+            ModelEmpEducationList objResult = new ModelEmpEducationList();
+
+            DataSet DS = new DataSet();
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                SqlCommand cmd = new SqlCommand("USP_SEL_ALL_Employees", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //-------------------------------------------------
+                // DATA ADAPTER
+                //-------------------------------------------------
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                da.Fill(DS);
+
+                //-------------------------------------------------
+                // CHECK MASTER TABLE
+                //-------------------------------------------------
+
+                if (DS.Tables.Count > 0 && DS.Tables[0].Rows.Count > 0)
+                {
+
+                    //-------------------------------------------------
+                    // LOOP EMPLOYEES
+                    //-------------------------------------------------
+
+                    foreach (DataRow dr in DS.Tables[0].Rows)
+                    {
+
+                        ModelEmployeeData employee = new ModelEmployeeData();
+
+                        employee.EMPGUID = dr["EMPGUID"].ToString();
+
+                        employee.EMPName = dr["EMPName"].ToString();
+
+                        employee.DepartmentID = dr["DepartmentID"].ToString();
+
+                        employee.MobileNo = dr["MobileNo"].ToString();
+
+                        employee.EmailID = dr["EmailID"].ToString();
+
+                        employee.JoinDate = dr["JoinDate"].ToString();
+
+                        employee.Salary = dr["Salary"].ToString();
+
+                        employee.Status = Convert.ToInt32(dr["Status"]);
+
+                        //-------------------------------------------------
+                        // FIND MATCHING EDUCATION ROWS
+                        //-------------------------------------------------
+
+                        if (DS.Tables.Count > 1 && DS.Tables[1].Rows.Count > 0)
+                        {
+
+                            foreach (DataRow drEdu in DS.Tables[1].Rows)
+                            {
+
+                                //-------------------------------------------------
+                                // MATCH USING EMPGUID
+                                //-------------------------------------------------
+
+                                if (drEdu["EMPGUID"].ToString() == employee.EMPGUID)
+                                {
+
+                                    ModelEmployeeducationDetails education = new ModelEmployeeducationDetails();
+
+                                    education.EDUGUID = drEdu["EDU_GUID"].ToString();
+
+                                    education.EMPGUID = drEdu["EMPGUID"].ToString();
+
+                                    education.Qualification = drEdu["Qualification"].ToString();
+
+                                    education.InstitutionName = drEdu["InstitutionName"].ToString();
+
+                                    education.YearOfPassing = drEdu["YearOfPassing"].ToString();
+
+                                    education.Percentage = drEdu["Percentage"].ToString();
+
+                                    education.Remarks = drEdu["Remarks"].ToString();
+
+                                    //-------------------------------------------------
+                                    // ADD CHILD
+                                    //-------------------------------------------------
+
+                                    employee.EducationDetails.Add(education);
+
+                                }
+
+                            }
+
+                        }
+
+                        //-------------------------------------------------
+                        // ADD EMPLOYEE
+                        //-------------------------------------------------
+
+                        objResult.MasterEducationData.Add(employee);
+
+                    }
+
+                    //-------------------------------------------------
+                    // SUCCESS RESPONSE
+                    //-------------------------------------------------
+
+                    objResult.Response = new ModelResponse("111", "Success", "Employee", "Data Retrieved Successfully", "");
+
+                }
+                else
+                {
+
+                    objResult.Response = new ModelResponse("222", "No Data", "Employee", "No Records Found", "");
+
+                }
+
+            }
+
+            return objResult;
+
+        }
+
+        [HttpGet]
+        [Route("GetEmployee")]
+
+        public ModelEmpEducationList GetEmployee(string EMPGUID)
+        {
+
+            ModelEmpEducationList objResult = new ModelEmpEducationList();
+
+            DataSet DS = new DataSet();
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+
+                SqlCommand cmd = new SqlCommand("USP_SEL_Employee", con);
+
+                cmd.CommandType =  CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue( "@guidEMPGUID", EMPGUID);
+
+                //-------------------------------------------------
+                // DATA ADAPTER
+                //-------------------------------------------------
+
+                SqlDataAdapter da =  new SqlDataAdapter(cmd);
+
+                da.Fill(DS);
+
+                //-------------------------------------------------
+                // CHECK MASTER TABLE DATA
+                //-------------------------------------------------
+
+                if (DS.Tables.Count > 0 && DS.Tables[0].Rows.Count > 0)
+                {
+
+                    //-------------------------------------------------
+                    // LOOP MASTER TABLE
+                    //-------------------------------------------------
+
+                    foreach (DataRow dr in DS.Tables[0].Rows) {
+
+                        ModelEmployeeData employee = new ModelEmployeeData();
+
+                        employee.EMPGUID = dr["EMPGUID"].ToString();
+
+                        employee.EMPName = dr["EMPName"].ToString();
+
+                        employee.DepartmentID = dr["DepartmentID"].ToString();
+
+                        employee.MobileNo = dr["MobileNo"].ToString();
+
+                        employee.EmailID = dr["EmailID"].ToString();
+
+                        employee.JoinDate = dr["JoinDate"].ToString();
+
+                        employee.Salary = dr["Salary"].ToString();
+
+                        employee.Status = Convert.ToInt32(dr["Status"]);
+
+                        //-------------------------------------------------
+                        // CHILD TABLE
+                        //-------------------------------------------------
+
+                        if (DS.Tables.Count > 1 && DS.Tables[1].Rows.Count > 0)
+                        {
+
+                            foreach (DataRow drEdu in DS.Tables[1].Rows)
+                            {
+
+                                ModelEmployeeducationDetails education = new ModelEmployeeducationDetails();
+
+                                education.EDUGUID = drEdu["EDU_GUID"].ToString();
+
+                                education.EMPGUID = drEdu["EMPGUID"].ToString();
+
+                                education.Qualification = drEdu["Qualification"].ToString();
+
+                                education.InstitutionName = drEdu["InstitutionName"].ToString();
+
+                                education.YearOfPassing = drEdu["YearOfPassing"].ToString();
+
+                                education.Percentage = drEdu["Percentage"].ToString();
+
+                                education.Remarks = drEdu["Remarks"].ToString();
+
+                                employee.EducationDetails.Add(education);
+
+                            }
+
+                        }
+
+                        //-------------------------------------------------
+                        // ADD TO FINAL LIST
+                        //-------------------------------------------------
+
+                        objResult.MasterEducationData.Add(employee);
+
+                    }
+
+                    //-------------------------------------------------
+                    // SUCCESS RESPONSE
+                    //-------------------------------------------------
+
+                    objResult.Response = new ModelResponse("111", "Success", "Employee", "Data Retrieved Successfully", "");
+
+                }
+
+                else
+                {
+
+                    objResult.Response = new ModelResponse("222", "No Data", "Employee", "No Records Found", "");
+
+                }
+
+            }
+
+            return objResult;
+
+        }
+
+
+
+        [HttpPost]
+        [Route("PostEmployee")]
+
+        public ModelEmployeeResponse PostEmployee(ModelEmployee employee) {
+
+            ModelEmployeeResponse objResult = new ModelEmployeeResponse();
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+
+                SqlCommand cmd = new SqlCommand("USP_IOU_Employee", con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //-------------------------------------------------
+                // MASTER PARAMETERS
+                //-------------------------------------------------
+
+                cmd.Parameters.AddWithValue("@guidEMPGUID", employee.EMPGUID);
+
+                cmd.Parameters.AddWithValue("@varEMPName", employee.EMPName);
+
+                cmd.Parameters.AddWithValue("@guidDepartmentID", employee.DepartmentID);
+
+                cmd.Parameters.AddWithValue("@varMobileNo", employee.MobileNo);
+
+                cmd.Parameters.AddWithValue("@varEmailID", employee.EmailID);
+
+                cmd.Parameters.AddWithValue("@dtmJoinDate", employee.JoinDate);
+ 
+                cmd.Parameters.AddWithValue("@decSalary", employee.Salary);
+
+                cmd.Parameters.AddWithValue("@tintStatus", employee.Status);
+
+                //-------------------------------------------------
+                // SAMPLE CREATED BY
+                //-------------------------------------------------
+
+                cmd.Parameters.AddWithValue("@guidCreatedBy", Guid.NewGuid());
+
+                //-------------------------------------------------
+                // XML CREATION
+                //-------------------------------------------------
+
+                string xmlEducation = "";
+
+                if (employee.Education.Count > 0)
+                {
+
+                    XmlDocument doc = new XmlDocument();
+
+                    XmlNode rootNode = doc.CreateElement("Table");
+
+                    doc.AppendChild(rootNode);
+
+                    for (int i = 0; i < employee.Education.Count; i++)
+                    {
+
+                        XmlNode rowNode = doc.CreateElement("Rows");
+
+                        XmlElement rowElement = (XmlElement)rowNode;
+
+                        rowElement.SetAttribute("EDU_GUID", employee.Education[i].EDUGUID);
+
+                        rowElement.SetAttribute("Qualification", employee.Education[i].Qualification);
+
+                        rowElement.SetAttribute("InstitutionName", employee.Education[i].InstitutionName);
+
+                        rowElement.SetAttribute("YearOfPassing", employee.Education[i].YearOfPassing);
+
+                        rowElement.SetAttribute("Percentage", employee.Education[i].Percentage);
+
+                        rowElement.SetAttribute("Remarks", employee.Education[i].Remarks);
+
+                        rootNode.AppendChild(rowNode);
+
+                    }
+
+                    xmlEducation = doc.InnerXml;
+
+                }
+
+                //-------------------------------------------------
+                // XML PARAMETER
+                //-------------------------------------------------
+
+                cmd.Parameters.AddWithValue( "@xmlEmployeeEducation", xmlEducation);
+
+                //-------------------------------------------------
+                // OUTPUT PARAMETER
+                //-------------------------------------------------
+
+                cmd.Parameters.Add("@chnOutputParameter", SqlDbType.NVarChar, 500);
+
+                cmd.Parameters["@chnOutputParameter"].Direction = ParameterDirection.Output;
+
+                //-------------------------------------------------
+                // EXECUTE
+                //-------------------------------------------------
+
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                string message = cmd.Parameters["@chnOutputParameter"].Value.ToString();
+
+                //-------------------------------------------------
+                // RESPONSE
+                //-------------------------------------------------
+
+                if (message == "Inserted")
+                {
+                    objResult.Response = new ModelResponse("111", "Success", "Employee", "Inserted Successfully", "");
+                }
+                else if (message == "Updated")
+                {
+                    objResult.Response =
+                        new ModelResponse("111","Success", "Employee", "Updated Successfully", "");
+                }
+                else if (message == "Exists")
+                {
+                    objResult.Response =
+                        new ModelResponse("222", "Exists", "Employee", "Employee Already Exists", "");
+                }
+                else
+                {
+                    objResult.Response = new ModelResponse("333", "Failed", "Employee", message, "" );
+                }
+
+            }
+
+            return objResult;
+
+        }
+
+    }
+}
--- WebApplication1/Models/Entities/Employee.cs
+++ WebApplication1/Models/Entities/Employee.cs
@@ -1,11 +1,163 @@
 ﻿namespace WebApplication1.Models.Entities
 {
+    //---------------------------------------------------------
+    // EXISTING CLASS - KEEP AS IT IS
+    //---------------------------------------------------------
+
     public class Employee
     {
         public Guid Id { get; set; }
+
         public required string Name { get; set; }
+
         public required string Email { get; set; }
+
         public string? Phone { get; set; }
+
         public decimal Salary { get; set; }
+    }
+
+    public class ModelEmpEducationList
+
+    {
+        public ModelResponse Response { get; set; }
+        public List<ModelEmployeeData> MasterEducationData { get; set; }  = new List<ModelEmployeeData>();
+
+    }
+
+    public class ModelEmployeeData
+    {
+        public string EMPGUID { get; set; }
+        public string EMPName { get; set; }
+        public string DepartmentID { get; set; }
+        public string MobileNo { get; set; }
+        public string EmailID { get; set; }
+        public string JoinDate { get; set; }
+        public string Salary { get; set; }
+        public int Status { get; set; }
+        public List<ModelEmployeeducationDetails> EducationDetails { get; set; } = new List<ModelEmployeeducationDetails>();
+    }
+
+    public class ModelEmployeeducationDetails
+    {
+        public string EDUGUID { get; set; }
+        public string EMPGUID { get; set; }
+        public string Qualification { get; set; }
+        public string InstitutionName { get; set; }
+        public string YearOfPassing { get; set; }
+        public string Percentage { get; set; }
+        public string Remarks { get; set; }
+        public int Status { get; set; }
+    }
+
+    //---------------------------------------------------------
+    // MAIN EMPLOYEE MODEL
+    //---------------------------------------------------------
+
+    public class ModelEmployee
+    {
+        public string EMPGUID { get; set; }
+
+        public string EMPName { get; set; }
+
+        public string DepartmentID { get; set; }
+
+        public string MobileNo { get; set; }
+
+        public string EmailID { get; set; }
+
+        public string JoinDate { get; set; }
+
+        public string Salary { get; set; }
+
+        public int Status { get; set; }
+
+        public List<ModelEducation> Education { get; set; }
+            = new List<ModelEducation>();
+    }
+
+    //---------------------------------------------------------
+    // CHILD EDUCATION MODEL
+    //---------------------------------------------------------
+
+    public class ModelEducation
+    {
+        public string EDUGUID { get; set; }
+
+        public string Qualification { get; set; }
+
+        public string InstitutionName { get; set; }
+
+        public string YearOfPassing { get; set; }
+
+        public string Percentage { get; set; }
+
+        public string Remarks { get; set; }
+
+        public int Status { get; set; }
+    }
+
+    //---------------------------------------------------------
+    // RESPONSE WRAPPER
+    //---------------------------------------------------------
+
+    public class ModelEmployeeResponse
+    {
+        public ModelResponse Response { get; set; }
+
+        public List<ModelEmployee> EmployeeList { get; set; }
+            = new List<ModelEmployee>();
+    }
+
+    //---------------------------------------------------------
+    // COMMON RESPONSE MODEL
+    //---------------------------------------------------------
+
+    public class ModelResponse
+    {
+        public string _status { get; set; }
+
+        public string _code { get; set; }
+
+        public string _Source { get; set; }
+
+        public string _Description { get; set; }
+
+        public string _Details { get; set; }
+
+        public string _TransID { get; set; }
+
+        public ModelResponse(string code,
+                             string status,
+                             string source,
+                             string Description,
+                             string Details)
+        {
+            _code = code;
+            _status = status;
+            _Source = source;
+            _Description = Description;
+            _Details = Details;
+        }
+
+        public ModelResponse(string code,
+                             string status,
+                             string source,
+                             string Description,
+                             string Details,
+                             string TransID)
+        {
+            _code = code;
+            _status = status;
+            _Source = source;
+            _Description = Description;
+            _Details = Details;
+            _TransID = TransID;
+        }
+
+        public ModelResponse()
+        {
+
+        }
     }
 }
